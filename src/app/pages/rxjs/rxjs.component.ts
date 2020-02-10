4ab6c17f723da1fc5a37512e935eb514aa0eaf2a
devnido <luis.nido@hotmail.com>
2020-02-10 14:38:08 -0300

    Promesas, Observables y metatags

---
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { retry, map, filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-rxjs',
+  templateUrl: './rxjs.component.html',
+  styles: []
+})
+export class RxjsComponent implements OnInit, OnDestroy {
+
+  subscription: Subscription;
+
+  constructor() {
+
+    this.subscription = this.regresaObservable().pipe(retry(2))
+      .subscribe(
+        numero => console.log('Subs ', numero),
+        err => console.log(err),
+        () => console.log('completado')
+      );
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy() {
+    console.log('La pagina se va a cerrar');
+
+    this.subscription.unsubscribe();
+
+  }
+
+
+  regresaObservable(): Observable<any> {
+
+    return new Observable(observer => {
+
+      let contador = 0;
+
+      const intervalo = setInterval(() => {
+
+        contador++;
+
+        const salida = {
+          valor: contador
+        };
+
+        observer.next(salida);
+
+
+        if (contador === 3) {
+          clearInterval(intervalo);
+          observer.complete();
+        }
+
+        // if (contador === 2) {
+        //   // clearInterval(intervalo);
+        //   observer.error('Auxilio');
+        // }
+
+      }, 1000);
+
+
+    }).pipe(
+      map((resp: any) => resp.valor),
+      filter((valor, index) => {
+        // console.log('Filter', valor, index);
+
+        if ((valor % 2) === 1) {
+          // impar
+          return true;
+        } else {
+          return false;
+        }
+
+      })
+    );
+
+
+  }
+
+}
